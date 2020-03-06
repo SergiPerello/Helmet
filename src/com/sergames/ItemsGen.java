80d13fdcaa13d78632ece3877465241ce0fb0bb8
Sergi Perello <sergi.perello@mataro.epiaedu.cat>
2020-03-06 17:59:57 +0100

    added random item

--- src/com/sergames/ItemsGen.java
+++ src/com/sergames/ItemsGen.java
@@ -10,13 +10,29 @@ public class ItemsGen {
         this.game = game;
     }
 
+    private ItemType randomItem() {
+        int pick = new Random().nextInt(ItemType.values().length);
+        return ItemType.values()[pick];
+    }
+
     int getRandom(int[] array) {
         int rnd = new Random().nextInt(array.length);
         return array[rnd];
     }
 
     void addItem() {
-        game.fallingObjects.add(new Item_Screwdriver(game, getRandom(positions), "/img/fallingObject-hammer.png"));
+        ItemType itemType = randomItem();
+        switch (itemType) {
+            case HAMMER:
+                game.fallingObjects.add(new Item_Hammer(game, getRandom(positions), "/img/fallingObject-hammer.png"));
+                break;
+            case SCREWDRIVER:
+                game.fallingObjects.add(new Item_Screwdriver(game, getRandom(positions), "/img/fallingObject-screwdriver.png"));
+                break;
+            case WRENCH:
+                break;
+        }
+
     }
 
     void deleteCollisionItems() {
